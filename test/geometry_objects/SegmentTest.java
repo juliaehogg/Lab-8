871c8d441dde96e9bce5d9690dd5c938916968b4
juliaehogg <122399945+juliaehogg@users.noreply.github.com>
2023-04-12 21:15:50 -0400

    added SegmentTest file

---
+++ test/geometry_objects/SegmentTest.java
@@ -0,0 +1,18 @@
+package geometry_objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SegmentTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+		
+		// HasSubSegment
+		// coincideWithoutOverlap
+		// collectOrderedPointsOnSegment
+	}
+
+}
