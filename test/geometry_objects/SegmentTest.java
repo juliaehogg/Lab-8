34811338e7c26e01e07f9be8d0ff472965ca34c2
gthouser <grace.houser@furman.edu>
2023-04-12 21:06:06 -0400

    added SegmentTest file

---
+++ test/geometry_objects/SegmentTest.java
@@ -0,0 +1,19 @@
+package geometry_objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SegmentTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+		
+		
+		// HasSubSegment
+		// coincideWithoutOverlap
+		// collectOrderedPointsOnSegment
+	}
+
+}
