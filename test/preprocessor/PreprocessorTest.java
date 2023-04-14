d7909504c508145d9dcaed3a8bb62b93c365ae02
gthouser <grace.houser@furman.edu>
2023-04-14 12:17:10 -0400

    updated testing

--- test/preprocessor/PreprocessorTest.java
+++ test/preprocessor/PreprocessorTest.java
@@ -19,6 +19,7 @@ import preprocessor.delegates.ImplicitPointPreprocessor;
 
 class PreprocessorTest
 {
+	// Testing a pentagon with an interesting star structure 
 	void test_implicit_crossings(String filename)
 	{
 		FigureNode fig = InputFacade.extractFigure(filename);
@@ -158,7 +159,6 @@ class PreprocessorTest
 	}
 	
 	
-	
 	@Test
 	void test_implicit_crossings() {
 		
@@ -166,9 +166,164 @@ class PreprocessorTest
 		
 		// add filenames to be tested 
 		filenames.add("fully_connected_irregular_polygon");
+		//filenames.add("intersecting_star");
 		
 		for (int i=0; i<filenames.size(); i++) {
 			test_implicit_crossings(filenames.get(i) + ".json");
+		}
+	}
+	
+	
+	
+	
+	
+	// Testing an interesting star structure 
+	
+	void test_implicit_crossings_intersecting_star(String filename)
+	{
+		FigureNode fig = InputFacade.extractFigure(filename);
+
+		Map.Entry<PointDatabase, Set<Segment>> pair = InputFacade.toGeometryRepresentation(fig);
+
+		PointDatabase points = pair.getKey();
+
+		Set<Segment> segments = pair.getValue();
+
+		Preprocessor pp = new Preprocessor(points, segments);
+
+		// 5 new implied points inside the pentagon
+		Set<Point> iPoints = ImplicitPointPreprocessor.compute(points, new ArrayList<Segment>(segments));
+		assertEquals(5, iPoints.size());
+
+
+		//
+		//
+		//		               D(3, 8)
+		//
+		//
+		//   E(-2,5)       D*      E*
+		//		         C*          A*       C(6, 5)
+		//                      B*
+		//		       A(0,0)        B(4, 0)
+		//
+		//		    An irregular pentagon with 5 C 2 = 10 segments
+
+		Point a_star = new Point(192.0 / 53.0, 480.0 / 159.0);
+		Point b_star = new Point(2, 5.0 / 3.0);
+		Point c_star = new Point(20.0 / 21.0, 160.0 / 63.0);
+		Point d_star = new Point(1.875, 5);
+		Point e_star = new Point(3.375, 5);
+
+		assertTrue(iPoints.contains(a_star));
+//		assertTrue(iPoints.contains(b_star));
+		assertTrue(iPoints.contains(c_star));
+		assertTrue(iPoints.contains(d_star));
+		assertTrue(iPoints.contains(e_star));
+
+		//
+		// There are 15 implied segments inside the pentagon; see figure above
+		//
+		Set<Segment> iSegments = pp.computeImplicitBaseSegments(iPoints);
+		assertEquals(15, iSegments.size());
+
+		List<Segment> expectedISegments = new ArrayList<Segment>();
+
+		expectedISegments.add(new Segment(points.getPoint("A"), c_star));
+		expectedISegments.add(new Segment(points.getPoint("A"), b_star));
+
+		expectedISegments.add(new Segment(points.getPoint("B"), b_star));
+		expectedISegments.add(new Segment(points.getPoint("B"), a_star));
+
+		expectedISegments.add(new Segment(points.getPoint("C"), a_star));
+		expectedISegments.add(new Segment(points.getPoint("C"), e_star));
+
+		expectedISegments.add(new Segment(points.getPoint("D"), d_star));
+		expectedISegments.add(new Segment(points.getPoint("D"), e_star));
+
+		expectedISegments.add(new Segment(points.getPoint("E"), c_star));
+		expectedISegments.add(new Segment(points.getPoint("E"), d_star));
+
+		expectedISegments.add(new Segment(c_star, b_star));
+		expectedISegments.add(new Segment(b_star, a_star));
+		expectedISegments.add(new Segment(a_star, e_star));
+		expectedISegments.add(new Segment(e_star, d_star));
+		expectedISegments.add(new Segment(d_star, c_star));
+
+		for (Segment iSegment : iSegments)
+		{
+			assertTrue(expectedISegments.contains(iSegment));
+		}
+
+		//
+		// Ensure we have ALL minimal segments: 20 in this figure.
+		//
+		List<Segment> expectedMinimalSegments = new ArrayList<Segment>(iSegments);
+		
+		Set<Segment> minimalSegments = pp.identifyAllMinimalSegments(iPoints, segments, iSegments);
+		
+		assertEquals(expectedMinimalSegments.size(), minimalSegments.size());
+
+		for (Segment minimalSeg : minimalSegments)
+		{
+			assertTrue(expectedMinimalSegments.contains(minimalSeg));
+		}
+		
+		//
+		// Construct ALL figure segments from the base segments
+		//
+		Set<Segment> computedNonMinimalSegments = pp.constructAllNonMinimalSegments(minimalSegments);
+		
+		//
+		// All Segments will consist of the new 15 non-minimal segments.
+		//
+		assertEquals(15, computedNonMinimalSegments.size());
+
+		//
+		// Ensure we have ALL minimal segments: 15 in this figure.
+		//
+		List<Segment> expectedNonMinimalSegments = new ArrayList<Segment>();
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("A"), d_star));
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("D"), c_star));
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("A"), points.getPoint("D")));
+		
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("B"), c_star));
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("E"), b_star));
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("B"), points.getPoint("E")));
+		
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("C"), d_star));
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("E"), e_star));
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("C"), points.getPoint("E")));		
+
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("A"), a_star));
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("C"), b_star));
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("A"), points.getPoint("C")));
+		
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("B"), e_star));
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("D"), a_star));
+		expectedNonMinimalSegments.add(new Segment(points.getPoint("B"), points.getPoint("D")));
+		
+		//
+		// Check size and content equality
+		//
+		assertEquals(expectedNonMinimalSegments.size(), computedNonMinimalSegments.size());
+
+		for (Segment computedNonMinimalSegment : computedNonMinimalSegments)
+		{
+			assertTrue(expectedNonMinimalSegments.contains(computedNonMinimalSegment));
+		}
+	}
+	
+	
+	@Test
+	void test_implicit_crossings_intersecting_star() {
+		
+		ArrayList<String> filenames = new ArrayList<String>();
+		
+		// add filenames to be tested 
+		filenames.add("intersecting_star");
+		
+		for (int i=0; i<filenames.size(); i++) {
+			test_implicit_crossings_intersecting_star(filenames.get(i) + ".json");
 		}
 	}
 }
